88998fd2d8774ed9a55b8bf1b0b81441ef4ce100
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-01-02 21:26:31 +0100

    Style navbar

--- src/componets/Header.vue
+++ src/componets/Header.vue
@@ -1,22 +1,28 @@
 <template>
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <RouterLink to="/start">Start</RouterLink>
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <RouterLink class="nav-link" to="/start">Start</RouterLink>
                 </li>
-                <li>
-                    <RouterLink to="/products">Products</RouterLink>
+                <li class="nav-item">
+                    <RouterLink class="nav-link" to="/products">Products</RouterLink>
                 </li>
-                <li>
-                    <RouterLink to="/categories">Categories</RouterLink>
+                <li class="nav-item">
+                    <RouterLink class="nav-link" to="/categories">Categories</RouterLink>
                 </li>
-                <li>
-                    <RouterLink to="/profile">Profile</RouterLink>
+                <li class="nav-item">
+                    <RouterLink class="nav-link" to="/profile">Profile</RouterLink>
                 </li>
             </ul>
-        </nav>
-    </header>
+                  </div>
+        </div>
+    </nav>
 </template>
 <script>
 import { RouterLink } from 'vue-router';
