9eba66b9e3a5eb3629b10d5d58e2796042380818
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-01-03 17:35:25 +0100

    Link to sign up view from login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,28 +1,31 @@
 <template>
-  <div class="container mt-5" style="max-width: 400px;">
-    <h1 class="mb-4 text-center">Login</h1>
+    <div class="container mt-5" style="max-width: 400px;">
+        <h1 class="mb-4 text-center">Login</h1>
 
-    <form>
-      <!-- Username input field -->
-      <div class="mb-3">
-        <label for="username" class="form-label">Username</label>
-        <input type="text" id="username" class="form-control" />
-      </div>
+        <form>
+            <!-- Username input field -->
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" id="username" class="form-control" />
+            </div>
 
-      <!-- Password input field -->
-      <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" id="password" class="form-control" />
-      </div>
+            <!-- Password input field -->
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" id="password" class="form-control" />
+            </div>
 
-      <!-- Submit button for login -->
-      <button type="submit" class="btn btn-primary w-100">Login</button>
-    </form>
-  </div>
+            <!-- Submit button for login -->
+            <button type="submit" class="btn btn-primary w-100">Login</button>
+            <!-- Signup link -->
+            <p class="mt-3 text-center">
+                Don't have an account?
+                <RouterLink :to= "{ name:'signup' }" class="text-decoration-none">Sign up</RouterLink>
+            </p>
+        </form>
+    </div>
 </template>
 <script setup>
 
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
