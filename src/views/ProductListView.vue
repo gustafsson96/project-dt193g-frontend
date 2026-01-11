7f8e2dd4306a5ff4d5515e1b13b4723341cf7b09
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-01-11 20:44:26 +0100

    Update products title to match category title

--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -2,7 +2,7 @@
   <div class="container mt-5">
     <!-- Add product button -->
     <div class="d-flex justify-content-between align-items-center mb-3">
-      <h1>Products List</h1>
+      <h1>Products</h1>
       <RouterLink :to="{ name: 'products/add' }" class="btn btn-success">
         <i class="fas fa-plus me-1"></i>
         Add Product
